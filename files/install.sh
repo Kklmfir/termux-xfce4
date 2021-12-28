40adc618a8577e072b668da58d5f6ea5f80e6786
Almer K <85006588+Kklmfir@users.noreply.github.com>
2021-12-28 21:03:49 +0700

    Update install.sh

--- files/install.sh
+++ files/install.sh
@@ -2,9 +2,11 @@
 
 # Move to Home
 mv .bashrc $HOME
+mv .vimrc $HOME
+mv .config $HOME
 mv .themes $HOME
 mv .icons $HOME
-mv .config $HOME
+mv .fonts $HOME
 mv .local $HOME
 
 # Success and How to connect to VNC
