a829ce4c677089d8ff3b7ee72b4ce89f717890bb
Almer K <85006588+Kklmfir@users.noreply.github.com>
2022-03-18 16:07:59 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,11 +6,11 @@ pkg install unstable-repo -y
 pkg install x11-repo -y
 
 # Install CLI Apps
-apt install moc vim emacs nano vi w3m curl tty-clock -y
+apt install moc vim wget nano vi w3m curl tty-clock htop neofetch-y
 
 # Install GUI Apps
 apt install vlc leafpad thunar telegram-desktop -y
 
 # Install GUI 
-cd file && chmod 777* && bash install.sh
+cd file && chmod 777* && ./install.sh
 clear && cd $HOME/termux-xfce4
